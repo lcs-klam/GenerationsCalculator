4bc088639166aac8e194a522710683ee1c72ef20
Kevin Lam <klam@lcs.on.ca>
2025-02-27 09:25:02 -0500

    Initial Commit

---
+++ GenerationCalculator/GenerationCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerationCalculatorApp.swift
+//  GenerationCalculator
+//
+//  Created by Kevin Lam on 27/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct GenerationCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
